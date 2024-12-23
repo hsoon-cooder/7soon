74814fe942c0eaf211fd9bfb4a96dbfd932626c0
hsoon-cooder <7soonalmshaweh99148021@gmail.com>
2024-12-23 20:55:32 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,27 @@
+// إظهار نافذة Cert
 document.addEventListener("DOMContentLoaded", function () {
-    // فتح نافذة Cert عند الضغط على الزر
     const certButton = document.querySelector("#cert-button");
     const modalCert = document.querySelector("#cert-modal");
     const closeButtons = document.querySelectorAll(".close-button");
+    const languageToggleButton = document.querySelector("#language-toggle");
 
-    // فتح نافذة Cert (Modal) عند الضغط على زر Cert
     certButton.addEventListener("click", function () {
         modalCert.style.display = "block"; // عرض نافذة Cert
     });
 
-    // إغلاق نافذة Cert عند الضغط على زر الإغلاق "X"
     closeButtons.forEach(button => {
         button.addEventListener("click", function () {
             modalCert.style.display = "none"; // إغلاق نافذة Cert
         });
     });
 
-    // إغلاق نافذة Cert عند الضغط خارج النافذة
     window.addEventListener("click", function(event) {
         if (event.target === modalCert) {
             modalCert.style.display = "none"; // إغلاق نافذة Cert عند الضغط خارجها
         }
     });
-});
+
+    // تغيير اللغة عند الضغط على زر تغيير اللغة
+    languageToggleButton.addEventListener("click", function () {
+        const currentLang = document.documentElement.lang;
+        if (currentLang === "ar")
